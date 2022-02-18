b49233321cd661d38db1a397463eae81ce372b79
jouureee <jouureee@gmail.com>
2022-02-18 21:21:56 +0900

    swift phoneList substr

--- Swift/PhoneList/main.swift
+++ Swift/PhoneList/main.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+func solve(phone_book: [String]) -> Bool {
+    var book = phone_book
+    var answer: Bool = true
+    book.sort()
+    
+    for i in 0..<book.count - 1 {
+        var target = book[i]
+        let range = book[i+1].startIndex..<book[i+1].endIndex
+        if(target == book[i][range]) {
+            return false
+        }
+    }
+    return answer;
+
+}
+
+
