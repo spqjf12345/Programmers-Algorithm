7fe43e1902b8f1287aae61528bf6eb75e9225b25
jouureee <jouureee@gmail.com>
2022-04-22 00:25:50 +0900

    Update code

--- C++/bucket_left.cpp
+++ C++/bucket_left.cpp
@@ -1,38 +1,33 @@
-//
-//  bucket_left.cpp
-//  SOMA👩🏻‍💻
-//
-//  Created by JoSoJeong on 2022/04/22.
-//
-
 #include <string>
 #include <vector>
 #include <iostream>
 #include <stack>
 
 using namespace std;
+
 bool check(string s) {
     stack<char> st;
     for(int i = 0; i < s.length(); i++){
         if (s[i] == '[' || s[i] == '(' || s[i] == '{'){
             st.push(s[i]);
         }else {
-            if st.empty() { return false;}
-            if ((s[i] == ']' && st.top() == '[') || (s[i] == '}' && st.top() == '{') && (s[i] == ')' && st.top() == '(') {
+            if (st.empty()) { return false; }
+            if ((s[i] == ']' && st.top() == '[') || (s[i] == '}' && st.top() == '{') || (s[i] == ')' && st.top() == '(')) {
                 st.pop();
             }else {
                 return false;
             }
         }
     }
-    return true;
+    return st.empty();
 }
 int solution(string s) {
     int answer = 0;
+    if (check(s)) { answer += 1; }
     for(int i = 1; i < s.length(); i++){
         string ss = s.substr(1, s.length() - 1) + s.substr(0, 1);
         if (check(ss)) {
-            answer += 1
+            answer += 1;
         }
         s = ss;
     }
