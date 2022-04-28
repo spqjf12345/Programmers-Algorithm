5e4d2e4a514b8f165234b42108c9c4ff1d9df3cf
jouureee <jouureee@gmail.com>
2022-04-28 19:33:35 +0900

    Add failRate 2019

---
+++ Swift/kakao/2019/blind/failRate/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  SOMA👩🏻‍💻
+//
+//  Created by JoSoJeong on 2022/04/28.
+//
+
+import Foundation
+//시간 초과
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var arr = [(Int, Double)]()
+    for i in 1..<N+1 {
+        let people = stages.filter { $0 >= i}
+        let parent = Double(people.count)
+        let child = Double(people.filter { $0 == i}.count)
+        let failRate:Double = Double(child / parent)
+        arr.append((i, failRate)) // i단계 실패율
+    }
+
+    arr = arr.sorted { (num1, num2) -> Bool in
+        if (num1.1 == num2.1) {
+            return num1.0 < num2.0
+        }
+        return num1.1 >= num2.1
+    }
+    
+    return arr.map { Int($0.0) }
+}
+
